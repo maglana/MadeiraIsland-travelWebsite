47cd4815dfd05f6e2d073aee04afb84e7c945a5f
maglana <m.srutkowska@gmail.com>
2024-01-24 15:34:33 +0100

    java script in nav-active

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector("nav");
+const navBtn = document.querySelector(".burger-btn");
+
+const handleNav = () => {
+	nav.classList.toggle("nav-active");
+};
+navBtn.addEventListener("click", handleNav);
